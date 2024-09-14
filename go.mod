0de2a06781b1b9770c1958881d435bafb8f84570
Rex Lee(李俊) <root@duguying.net>
2024-09-14 12:24:16 +0800

    支持排序keys

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gogather/safemap/v2
+
+go 1.20
+
+require github.com/gogather/com v1.0.3
--- map.go
+++ map.go
@@ -1,33 +1,47 @@
 package safemap
 
-import "sync"
-import "fmt"
-import "github.com/gogather/com"
+import (
+	"fmt"
+	"sort"
+	"sync"
+
+	"github.com/gogather/com"
+)
 
 // SafeMap safe map struct
 type SafeMap struct {
 	sync.RWMutex
-	M map[string]interface{} `json:"_"`
+	m map[string]interface{}
 }
 
 // New new a SafeMap
 func New() *SafeMap {
 	return &SafeMap{
-		M: make(map[string]interface{}),
+		m: make(map[string]interface{}),
 	}
 }
 
 // Put put element into safemap
 func (sm *SafeMap) Put(key string, value interface{}) {
 	sm.Lock()
-	sm.M[key] = value
+	sm.m[key] = value
 	sm.Unlock()
+}
+
+// Keys sorted keys
+func (sm *SafeMap) Keys() []string {
+	var keys []string
+	for k := range sm.m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
 
 // Remove remove element from safemap
 func (sm *SafeMap) Remove(key string) {
 	sm.Lock()
-	delete(sm.M, key)
+	delete(sm.m, key)
 	sm.Unlock()
 }
 
@@ -37,7 +51,7 @@ func (sm *SafeMap) Get(key string) (interface{}, bool) {
 		sm.RUnlock()
 	}()
 	sm.RLock()
-	v, ok := sm.M[key]
+	v, ok := sm.m[key]
 	return v, ok
 }
 
@@ -46,7 +60,7 @@ func (sm *SafeMap) String() string {
 		sm.RUnlock()
 	}()
 	sm.RLock()
-	return fmt.Sprintf("%v", sm.M)
+	return fmt.Sprintf("%v", sm.m)
 }
 
 // JSON convert map to json string
@@ -55,16 +69,16 @@ func (sm *SafeMap) JSON() (json string) {
 		sm.RUnlock()
 	}()
 	sm.RLock()
-	json, _ = com.JsonEncode(sm.M)
+	json, _ = com.JsonEncode(sm.m)
 	return
 }
 
 // GetMap get original map
 func (sm *SafeMap) GetMap() map[string]interface{} {
-	return sm.M
+	return sm.m
 }
 
 // Clear clear the map
 func (sm *SafeMap) Clear() {
-	sm.M = make(map[string]interface{})
+	sm.m = make(map[string]interface{})
 }
